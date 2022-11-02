1f3fbfbfcc1ab34ceab0d27c6e8a2656a5a01f0c
Yegor <egormonte2015@gmail.com>
2022-11-02 19:22:11 +0100

    start make carousel

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: <button type="button" class="slick-prev"><img src="../left_solid.png"></img></button>,
+        nextArrow: <button type="button" class="slick-next"><img src="../right_solid.png"></img></button>,
+    });
+  });
