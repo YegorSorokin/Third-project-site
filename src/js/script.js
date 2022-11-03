2e942348811355541fe06f274047986b358450ac
Yegor <egormonte2015@gmail.com>
2022-11-03 16:48:15 +0100

    made catalog

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: <button type="button" class="slick-prev"><img src="../left_solid.png"></img></button>,
-        nextArrow: <button type="button" class="slick-next"><img src="../right_solid.png"></img></button>,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left_solid.png"></img></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right_solid.png"></img></button>',
     });
   });
