5b25d9bb6b46e33c9c22b002370d784c72734dfe
Yegor <egormonte2015@gmail.com>
2022-11-08 18:15:52 +0100

    made animation of reviews and validation site

--- src/js/script.js
+++ src/js/script.js
@@ -73,17 +73,34 @@ $(document).ready(function() {
         if(!$(this).valid()) {
             return;
         }
-        
+
         $.ajax({
             type: "POST",
             url: "mailer/smart.php",
             data: $(this).serialize()
         }).done(function() {
             $(this).find("input").val("");
-
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
 
             $('form').trigger('reset');
         });
         return false;
     });
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    })
+
+    $("a[href^='#']").click(function() {
+        const _href = $(this).attr("href")
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    })
+
+    new WOW().init();
 });
